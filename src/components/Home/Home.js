b69eefbb128a48b91c6f97065d6991004bdada04
Peter <peter.zmilczak@gmail.com>
2021-09-05 11:47:38 +0200

    some small improvements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,34 @@
-import React,{ useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
-import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../../constants/apiConstants';
+import React, {useEffect} from 'react';
+import {withRouter} from 'react-router-dom';
+import {ACCESS_TOKEN_NAME, API_BASE_URL} from '../../constants/apiConstants';
 import axios from 'axios'
+
 function Home(props) {
     useEffect(() => {
-        // axios.get(API_BASE_URL+'/products', { headers: { 'token': localStorage.getItem(ACCESS_TOKEN_NAME) }})
-        // .then(function (response) {
-        //     if(response.status !== 200){
-        //       redirectToLogin()
-        //     }
-        // })
-        // .catch(function (error) {
-        //   redirectToLogin()
-        // });
-      })
+        axios.get(API_BASE_URL + '/products', {
+            headers: {
+                'token': localStorage.getItem(ACCESS_TOKEN_NAME)
+            },
+            withCredentials: true
+        })
+            .then(function (response) {
+                if (response.status === 200) {
+                    console.log(response.data)
+                }
+                if (response.status !== 200) {
+                    redirectToLogin()
+                }
+            })
+            .catch(function (error) {
+                redirectToLogin()
+            });
+    })
+
     function redirectToLogin() {
-    props.history.push('/login');
+        props.history.push('/login');
     }
-    return(
+
+    return (
         <div className="mt-2">
             Home page content
         </div>
